df2c51912ef03d83af586627107081d1f3391dad
Marco Ruiz <marco.ruiz.di@gmail.com>
2024-10-30 14:39:01 -0300

    se crea listado, estado vuex e interfaz de tabla

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="title">Tienda 32 bits</h1>
+    <table class="table-juegos">
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Nombre</th>
+          <th>Stock</th>
+          <th>Precio</th>
+          <th>Color</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="juego in juegos" :key="juego.codigo">
+          <td>{{ juego.codigo }}</td>
+          <td>{{ juego.nombre }}</td>
+          <td>{{ juego.stock }}</td>
+          <td>{{ juego.precio }}</td>
+          <td>{{ juego.color }}</td>
+          <td>
+            <button @click="incrementarStock(juego.codigo)">+</button>
+            <button @click="disminuirStock(juego.codigo)">-</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState, mapActions } from "vuex";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  computed: {
+    ...mapState(["juegos"]),
+  },
+  methods: {
+    ...mapActions(["incrementarStock", "disminuirStock"]),
+  },
+};
 </script>
 
 <style>
@@ -22,5 +50,44 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  width: 80%;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.table-juegos {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+
+.table-juegos th,
+.table-juegos td {
+  padding: 10px; /* Espacio entre el contenido de la celda y el borde */
+  border: 1px solid #ccc; /* Borde de la tabla */
+}
+
+.table-juegos th {
+  background-color: #f0f0f0; /* Color de fondo para los encabezados */
+  font-weight: bold; /* Texto en negrita para los encabezados */
+}
+
+.table-juegos tbody tr:nth-child(even) {
+  background-color: #f8f8f8; /* Color de fondo alternativo para las filas pares */
+}
+
+.title {
+  font-size: 2em;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.subtitle {
+  font-size: 1.5em;
+  font-weight: bold;
+  margin-bottom: 20px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,68 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
-export default createStore({
+const store = createStore({
   state: {
-    contador: 1,
-    nombre: 'Javier',
-    carrera: "Front End Vue"
-  },
-  getters: {
+    juegos: [
+      {
+        codigo: "0001",
+        nombre: "Sekiro",
+        stock: 43,
+        precio: 30000,
+        color: "red"
+      },
+      {
+        codigo: "0002",
+        nombre: "Fifa 21",
+        stock: 55,
+        precio: 25000,
+        color: "blue"
+      },
+      {
+        codigo: "0003",
+        nombre: "Gears of War 4",
+        stock: 12,
+        precio: 15000,
+        color: "green"
+      },
+      {
+        codigo: "0004",
+        nombre: "Mario Tennis Aces",
+        stock: 56,
+        precio: 35000,
+        color: "black"
+      },
+      {
+        codigo: "0005",
+        nombre: "Bloodborne",
+        stock: 12,
+        precio: 10000,
+        color: "blue"
+      },
+      {
+        codigo: "0006",
+        nombre: "Forza Horizon 4",
+        stock: 4,
+        precio: 20000,
+        color: "red"
+      }
+    ]
   },
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    modificarStock(state, { codigo, cantidad }) {
+        const juego = state.juegos.find((juego) => juego.codigo === codigo);
+        if (juego && juego.stock + cantidad >= 0) {
+            juego.stock += cantidad;
+        }
+    },
+},
+actions: {
+    incrementarStock({ commit }, codigo) {
+        commit("modificarStock", { codigo, cantidad: 1 });
+    },
+    disminuirStock({ commit }, codigo) {
+        commit("modificarStock", { codigo, cantidad: -1 });
+    },
+},
+});
+
+export default store;
