de6dc24c9e85657567bdbe2c39f9f7691cf8f6a6
Marco Ruiz <marco.ruiz.di@gmail.com>
2024-10-30 14:00:15 -0300

    dependencias instaladas, node_modules fuera

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    contador: 1,
+    nombre: 'Javier',
+    carrera: "Front End Vue"
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
